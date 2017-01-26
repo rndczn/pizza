f755ca24dd208edb1830c29a0fa8e1f28105c9f3
pvchaumier <chaumierpv@gmail.com>
2017-01-26 19:24:34 +0100

    Parser

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 """Main for hashcode test."""
 
-FILE = 'small.in'
+import numpy as np
+
+FILE = 'data/example.in'
 
 def parse(file):
     with open(file) as f:
         for i, line in enumerate(f):
-            print(i, line)
+            if i == 0:
+                R, C, L, H = (int(el) for el in line.split())
+                pizza = np.zeros((R, C))
+                continue
+            for j in range(C):
+                # T = 1, M = 0
+                pizza[i-1, j] = (line[j] == 'T')
+    return R, C, L, H, pizza
+
 
 if __name__ == '__main__':
-    parse(FILE)
+    R, C, L, H, pizza = parse(FILE)
+    print('R', R)
+    print('C', C)
+    print('L', L)
+    print('H', H)
+    print('pizza', pizza)
