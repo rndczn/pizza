d1de1efb42a7e78d9becd1e3103f343087ea269b
pvchaumier <chaumierpv@gmail.com>
2017-02-23 19:34:10 +0100

    test

--- selection/main.py
+++ selection/main.py
@@ -3,28 +3,59 @@ import unittest
 import numpy as np
 import pandas as pd
 
-FILE = ''
-
+FILE = 'sample'
+# FILE = 'kittens'
+# FILE = 'me_at_the_zoo'
+# FILE = 'trending_today'
+# FILE = 'videos_worth_spreading'
+FILENAME = 'data/' + FILE + '.in'
 
 #
 # CODE
 #
 def parse(filename):
     with open(filename) as f:
-        pass
+        V, E, R, C, X = map(int, f.readline().split())
+        endpoints = []
+        caches = []
+
+        vids = list(map(int, f.readline().split()))
+
+        for _ in range(E):
+            Ld, K = list(map(int, f.readline().split()))
+            endpoint = EndPoint(Ld, K, R, C)
+            for _ in range(K):
+                c, Lc = list(map(int, f.readline().split()))
+                endpoint.cache_servers_list[c] = Lc
+                endpoint.cache_servers.append(c)
+            endpoints.append(endpoint)
+
+        for _ in range(R):
+            Rv, Re, Rn = list(map(int, f.readline().split()))
+            endpoints[Re].requests[Rv] = Rn
 
 
-class Video(object):
-
-    def __init__(self, size, popu):
-        self.size = size
-        self.popu = popu
+    return V, E, R, C, X, vids, endpoints, requests
 
 
-class Server(object):
+class EndPoint(object):
 
-    def __init__(self, capacity):
-        self.capacity = capacity
+    def __init__(self, Ld, K, R, C):
+        self.Ld = Ld
+        self.K = K
+        self.cache_servers_list = [Ld] * C
+        self.cache_servers = []
+        self.requests = [0] * R
+
+    def __repr__(self):
+        return 'Ld:{Ld}/K:{K}/X:{X} => {cache_servers_list}'.format(**self.__dict__)
+
+
+class Cache(object):
+
+    def __init__(self, V):
+        self.endpoints = []
+        self.vids = [0] * V
 
 
 #
@@ -48,8 +79,18 @@ class TestSelection(unittest.TestCase):
         self.common = 10
 
 
-# 
+#
 # LAUNCHING
-# 
+#
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    V, E, R, C, X, vids, endpoints, requests = parse(FILENAME)
+    print()
+    print('V', V)
+    print('E', E)
+    print('R', R)
+    print('C', C)
+    print('X', X)
+    print('vids', vids)
+    print('endpoints', endpoints)
+    print('requests', requests)
